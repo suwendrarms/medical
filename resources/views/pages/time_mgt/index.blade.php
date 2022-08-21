8da935f6e5f65c5d26458f0cfcaf0b73ff89bc1b
Hiruni Perera <hiruni@weblankan.lk>
2022-08-22 05:22:16 +0530

    new commit

--- resources/views/pages/time_mgt/index.blade.php
+++ resources/views/pages/time_mgt/index.blade.php
@@ -117,7 +117,7 @@
 		<div class="modal-content">
 
 			<div class="modal-header">
-				<h5 class="modal-title">Add Drug</h5>
+				<h5 class="modal-title">Add Time Slot</h5>
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<i aria-hidden="true" class="ki ki-close"></i>
 				</button>
